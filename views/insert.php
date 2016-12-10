22742bdd2a8658014b88c8485c29df3d3aaf1540
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2016-12-10 15:44:18 -0500

    insert

---
+++ views/insert.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scrumptious</title>
+	<link href="/css/parent.css" rel="stylesheet" type="text/css"/>
+	<link href="/css/style.css" rel="stylesheet" type="text/css"/>
+	<link href="/img/Logo.png" rel="icon"/>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="/js/full-screen.js" type="text/javascript"></script>
+</head>
+
+<body>
+	<nav>	<!--Contains logo, searchbar, profile icon, bookmarks-->
+		<img src="/img/LogoFill.png">
+		<img src="/img/Bookmark.png">
+		<input type="text" class="main-search"/>
+	</nav>
+
+	<footer>
+		<p>Camden Hill 2016</p>
+	</footer>
+
+</body>
+</html>
