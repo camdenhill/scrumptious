a67b789afa414fcff5bd35f98cbe244293cc74c4
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2016-12-10 23:40:39 -0500

    get recipeID

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ module.exports = function(app)
 
 	app.get('/recipes/:recipeID', function (req, res) {
 		db.getRecipe(req.params.recipeID);
+		res.render('recipes.html')
+		document.getElementById('step1').textContent = 'step1';
 	})
 
 	app.get('/insert', function (req, res) {
