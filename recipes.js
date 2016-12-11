0b3abf472410c78db1e898cde5dcdac315bf43f9
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2016-12-11 00:45:27 -0500

    recipes

--- recipes.js
+++ recipes.js
@@ -1,5 +1,33 @@
 exports.renderRecipe = function(res, results) {
-	res.write('<html><head></head><body>');
+	res.write('
+		<body ng-app="app">
+			<nav>	<!--Contains logo, searchbar, profile icon, bookmarks-->
+				<img src="/img/LogoFill.png">
+				<img src="/img/Bookmark.png">
+				<input type="text" class="main-search"/>
+			</nav>
+
+			<div class="upper">	<!--Contains icon, title, rating, comments icon, prompt to serve #, dropdown serve #-->
+				<img src="/img/Pie.png">
+				<h1>Chicken Pot Pie</h1>
+				<ul class="ratings-and-comment">
+					<li><img src="/img/StarFull.png"</li>
+					<li><img src="/img/StarFull.png"</li>
+					<li><img src="/img/StarFull.png"</li>
+					<li><img src="/img/StarFull.png"</li>
+					<li><img src="/img/StarHalf.png"</li>
+					<li><img src="/img/Comment.png"</li>
+				</ul>
+				<div class="how-many-serve-div">
+					<h3 class="how-many-serve-h3">How many people do you want to serve?</h3>
+					<select class="how-many-serve-select">
+						<% for(var i=0; i<=12; i++) { %>
+							<option><%= i %></option>
+						<% } %>
+					</select>
+				</div>
+			</div>
+	');
 	res.write('<p>' + results[0]['recipeName'] + '</p>');
 	res.write('</body></html>');
 }
