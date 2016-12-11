204a947e59806bc5d49e625adda7a488102f3d1b
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2016-12-11 00:41:01 -0500

    recipes

--- recipes.js
+++ recipes.js
@@ -1,3 +1,5 @@
-res.write('<html><head></head><body>');
-res.write('<p>' + results[0].recipeID + '</p>');
-res.write('</body></html>');
+exports.renderRecipe = function(res, results) {
+	res.write('<html><head></head><body>');
+	res.write('<p>' + results[0].recipeID + '</p>');
+	res.write('</body></html>');
+}
