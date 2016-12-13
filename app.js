37d3f2e71149f595cb3af17126c2b2ad532f3dea
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2016-12-12 23:20:37 -0500

    source

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ module.exports = function(app)
 							steps : steps,
 							recipeName : ingredients[0]['recipeName'],
 							source : ingredients[0]['recipeSource'],
-							color : ingredients[0]['colorHex'],
+							color : "/css/colors/"+ingredients[0]['colorHex']+".css",
 							time : time
 						});		
 					}
