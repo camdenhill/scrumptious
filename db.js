3ba429a882424896e5e4f9945c860488842baa10
Camden Hill <camdenhill@Camdens-MacBook-Pro.local>
2016-12-18 10:18:00 -0800

    step duration on steps

--- db.js
+++ db.js
@@ -138,7 +138,7 @@ exports.getRecipeSteps = function(recipeID, callback) {
 	var data;
 	connection.query(sql, recipeID, function (err, res) {
 		connection.end();
-		console.log(res[0]['stepStart']);
+		console.log(explode(":", res[0]['stepStart'])[1]);
 		callback(err, res);
 	});
 }
