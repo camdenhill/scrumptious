5e2807eee4e713e418fca60e400cb558ac225840
Camden Hill <camdenhill@Camdens-MacBook-Pro.local>
2017-01-16 11:22:55 -0500

    home page

--- showcase.js
+++ showcase.js
@@ -7,7 +7,7 @@ var showcase = {
 	],
 	featured : '3',
 	gallery : '(2, 3)',
-	galleryTitles : ["Featured recipe of the day", "So hot right now"]
+	galleryTitles : '("Featured recipe of the day", "So hot right now")'
 };
 
 module.exports = showcase;
