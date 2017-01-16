9a834584c2b545225ed3e7031ad947870704623b
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2017-01-16 15:17:48 -0500

    home page

--- showcase.js
+++ showcase.js
@@ -3,8 +3,8 @@
 var showcase = {
 	categories : '(1, 2, 3, 4, 5)',	// Category IDs
 	featured : '3',					// Recipe IDs
-	gallery : '(2, 3)',				// Recipe IDs
-	galleryTitles : ["Featured recipe of the day", "So hot right now"]
+	gallery : '(2, 3, 7)',				// Recipe IDs
+	galleryTitles : ["Featured recipe of the day", "Hot right now", "What we're cooking"]
 };
 
 module.exports = showcase;
