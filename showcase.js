5965637749209debce23dc3a2b6242f86a6f72b2
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2017-01-15 23:10:41 -0500

    updating home page

--- showcase.js
+++ showcase.js
@@ -4,7 +4,7 @@ var showcase = {
 	categories : [
 		{ name : "Beat the cold", icon : "" },
 		{ name : "Date night", icon : "" }
-	]
+	],
 	featured : '3',
 	gallery : '(2, 3)',
 	galleryTitles : ["Featured recipe of the day", "So hot right now"]
