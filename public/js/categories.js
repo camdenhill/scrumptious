18600aa8cdc35445ff4d98334ad76029588219e0
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2017-01-21 16:05:01 -0500

    home page

---
+++ public/js/categories.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+	$('.category').css('opacity', .3);
+	$('.category:first-of-type').css('opacity', 1);
+});
--- showcase.js
+++ showcase.js
@@ -2,7 +2,7 @@
 
 var showcase = {
 	categories : '(1, 2, 3, 4, 5)',	// Category IDs
-	featured : '3',					// Recipe IDs
+	category : '3',					// Recipe IDs
 	gallery : '(2, 3, 7)',			// Recipe IDs
 	galleryTitles : ["Featured recipe of the day", "Hot right now", "What we're cooking"]
 };
