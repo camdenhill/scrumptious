b3d2ffa24f6ab7bcbcd581cdcca85717eafc8c22
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2017-01-21 16:07:19 -0500

    home page

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
 	$('.category').css('opacity', .3);
+	$('img[src="/img/Triangle.png"]').css('opacity', 0)
 	$('.category:first-of-type').css('opacity', 1);
+	$('.category:first-of-type img[src="/img/Triangle.png"]').css('opacity', 1);
 });
