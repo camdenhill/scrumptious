8313abf1b9b0120c7d64361a2272e50a34b1a852
Camden Hill <camdenhill@Camdens-MBP.fios-router.home>
2017-01-22 22:05:02 -0500

    new routes and pages for inserting

--- showcase.js
+++ showcase.js
@@ -3,7 +3,7 @@
 var showcase = {
 	categories : '(1, 2, 3, 4, 5)',	// Category IDs
 	category : '3',					// Recipe IDs
-	featured : '(2, 3, 7, 8)',
+	featured : '(2, 7, 3, 8)',
 	gallery : '(2, 3, 7)',			// Recipe IDs
 	galleryTitles : ["Featured recipe of the day", "Hot right now", "What we're cooking"]
 };
